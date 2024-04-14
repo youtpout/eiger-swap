18539e70fbdd8cc518f07a85ace6104ef9a3e225
Youtpout <EddyBoughioul@hotmail.fr>
2024-04-14 15:33:29 +0200

    add interfaces eigerswap

---
+++ src/EigerSwap.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+import "./interfaces/ERC20Swapper.sol";
+
+contract EigerSwap is ERC20Swapper {
+
+    constructor(address _owner){
+        
+    }
+}
---
+++ src/interfaces/ERC20Swapper.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+interface ERC20Swapper {
+    /// @dev swaps the `msg.value` Ether to at least `minAmount` of tokens in `address`, or reverts
+    /// @param token The address of ERC-20 token to swap
+    /// @param minAmount The minimum amount of tokens transferred to msg.sender
+    /// @return The actual amount of transferred tokens
+    function swapEtherToToken(address token, uint minAmount) public payable returns (uint);
+}
